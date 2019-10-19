df009ef3147b988005c36249599849e6e3ef5a2c
Matthieu Hostache <matthieu.hostache@gmail.com>
2019-10-19 14:11:25 +0200

    Remove logs

--- gsuite/lib/complete-oauth.js
+++ gsuite/lib/complete-oauth.js
@@ -19,7 +19,6 @@ async function completeOAuthProcess({ code, welinaClient, metadata }) {
 
 
     metadata.googleTokenInfo = response.tokens;
-    console.log('response.tokens', response.tokens);
 
     auth.setCredentials(metadata.googleTokenInfo);
     const domains = await getDomains(metadata.googleTokenInfo);
