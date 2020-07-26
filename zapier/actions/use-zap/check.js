fd94723adabbb9c71e2ac2ec11bd30e8eab58bc6
Matthieu Hostache <matthieu.hostache@gmail.com>
2020-07-26 15:51:21 +0200

    Fix Zapier integration invite link

--- zapier/actions/use-zap/check.js
+++ zapier/actions/use-zap/check.js
@@ -9,7 +9,7 @@ module.exports = withUiHook(async ({ payload, welinaClient }) => {
       <P>A Zap can now be created referencing this Welina Zap #ID: <strong>${shortId}</strong></P>
       <Input name="zapId" type="hidden" value="${shortId}" disabled />
 
-      <Button mb="6" as="a" variant="invert-blue" href="https://zapier.com/developer/zap-templates/create" target="_blank">
+      <Button mb="6" as="a" variant="invert-blue" href="https://zapier.com/developer/public-invite/111971/e58b589f96cc0e3f5279dd59fdd03e83/" target="_blank">
         Create a Zap using Welina Integration
       </Button>
       <Button action="submit">Continue</Button>
